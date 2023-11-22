2fb96ac9ed8c88674ae81a9ebb7a3bd506650643
MatheusBiazzi <matheus.biazzi@escola.pr.gov.br>
2023-11-21 23:10:50 -0300

    123

---
+++ JS/reg_3tri/main.js
@@ -0,0 +1,35 @@
+//amaterasu
+function falaAmaterasu(){
+    document.querySelector("#fala_amat").play();
+}
+document.querySelector(".fala1").onclick =falaAmaterasu;
+//Shinra tensei
+function falaSTensei(){
+    document.querySelector("#fala_stensei").play();
+}
+document.querySelector(".fala2").onclick =falaSTensei;
+//kagura
+function falaKagura(){
+    document.querySelector("#fala_kagu").play();
+}
+document.querySelector(".fala3").onclick =falaKagura;
+//kamehameha
+function falaKamehameha(){
+    document.querySelector("#fala_kame").play();
+}
+document.querySelector(".fala4").onclick =falaKamehameha;
+//Risada Kyra
+function falaLaught(){
+    document.querySelector("#fala_laught").play();
+}
+document.querySelector(".fala5").onclick =falaLaught;
+//rasegan
+function falaRasegan(){
+    document.querySelector("#fala_rase").play();
+}
+document.querySelector(".fala6").onclick =falaRasegan;
+//chidori
+function falaChidori(){
+    document.querySelector("#fala_chid").play();
+}
+document.querySelector(".fala7").onclick =falaChidori;
